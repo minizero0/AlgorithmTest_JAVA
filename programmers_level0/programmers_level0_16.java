a6a353d4867c6c917926e9fce69db4e3d9f3be64
yeongmin <dudals1069@gmail.com>
2023-03-10 16:37:46 +0900

    짝수는 싫어요

---
+++ programmers_level0/programmers_level0_16.java
@@ -0,0 +1,28 @@
+package programmers_level0;
+//        짝수는 싫어요
+
+//정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+public class programmers_level0_16 {
+    public static int[] solution(int n) {
+        int arr[];
+        if (n % 2 == 0){
+            arr = new int[n/2];
+        }else{
+            arr = new int[n/2+1];
+        }
+
+        int j = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 1){
+                arr[j++] = i;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+
+    }
+}
