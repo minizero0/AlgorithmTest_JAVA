8874212f2b992b853ccde5b9cb53a1f642703025
yeongmin <dudals1069@gmail.com>
2023-03-15 17:13:00 +0900

    특정 문자 제거하기

---
+++ programmers_level0/programmers_level0_25.java
@@ -0,0 +1,16 @@
+package programmers_level0;
+//          특정 문자 제거하기
+
+//        문자열 my_string과 문자 letter이 매개변수로 주어집니다.
+//        my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+public class programmers_level0_25 {
+    public static String solution(String my_string, String letter) {
+        String answer = my_string.replace(letter, "");
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("BCBdbe", "B"));
+
+    }
+}
