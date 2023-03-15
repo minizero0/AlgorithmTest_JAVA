fce1e45b6fb14a8fff6d04f65be7c5bb84caa88e
yeongmin <dudals1069@gmail.com>
2023-03-15 15:47:35 +0900

    순서쌍의 개수

--- programmers_level0/programmers_level0_19.java
+++ programmers_level0/programmers_level0_19.java
@@ -1,22 +1,23 @@
 package programmers_level0;
-//          배열 원소의 길이
+//          순서쌍의 개수
 
-//        문자열 배열 strlist가 매개변수로 주어집니다.
-//        strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+//        순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+//        자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
 
 public class programmers_level0_19 {
-    public static int[] solution(String[] strlist) {
-        int answer[] = new int[strlist.length];
-        for(int i = 0; i < strlist.length; i++){
-            answer[i] = strlist[i].length();
-        }
+    public static int solution(int n) {
+        int answer = 0;
 
+        for(int i = 1; i <=n; i++){
+            if(n % i == 0){             //i가 n과의 나머지가 0이면 순서쌍에 만족
+                answer++;
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        String arr[] = {"We", "are", "the", "world!"};
-        System.out.println(solution(arr));
+        System.out.println(solution(100));
 
     }
 }
