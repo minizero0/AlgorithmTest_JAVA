0e2a22c3b59e38c14ebf07f5a39d122f0d0bc9cd
yeongmin <dudals1069@gmail.com>
2023-03-20 17:45:02 +0900

    음양 더하기

--- programmers_level1/programmers_level1_16.java
+++ programmers_level1/programmers_level1_16.java
@@ -1,32 +1,21 @@
 package programmers_level1;
-//        핸드폰 번호 가리기
+//        음양 더하기
 
-//  프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
-//  전화번호가 문자열 phone_number로 주어졌을 때,
-//  전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+//  어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열
+//  signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
 
-import java.util.Arrays;
 
 public class programmers_level1_16 {
-    public static String solution(String phone_number) {
-        int cnt = phone_number.length();
-        String answer ="";
-        for(int i = 0; i < cnt; i++){
-            if(i + 4 == cnt){
-                for(int j = i; j < cnt; j++){
-                    answer += phone_number.charAt(j);
-                }
-                break;
-            }
-            answer += "*";
-
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer =0;
+        for(int i = 0; i < absolutes.length; i++){
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
         return answer;
-
     }
 
 
     public static void main(String[] args) {
-        System.out.println(solution("027778888"));
+        System.out.println(solution(new int[] {4,7,12}, new boolean[] {true, false, true}));
     }
 }
