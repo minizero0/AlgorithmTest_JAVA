bc125e5a1fe6b3e4a3ea9bf6c517e4261f91f185
yeongmin <dudals1069@gmail.com>
2023-03-21 23:30:18 +0900

    없는 숫자 더하기

---
+++ programmers_level1/programmers_level1_17.java
@@ -0,0 +1,21 @@
+package programmers_level1;
+//        없는 숫자 더하기
+
+//  0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다.
+//  numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+public class programmers_level1_17 {
+    public static int solution(int[] numbers) {
+        int answer = 45;
+        for(int i = 0; i < numbers.length; i++){
+            answer -= numbers[i];
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1,2,3,4,6,7,8,0}));
+    }
+}
