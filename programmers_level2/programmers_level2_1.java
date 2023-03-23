c7db23707b465b9bb3e1237363480cf056b096ba
yeongmin <dudals1069@gmail.com>
2023-03-23 16:54:47 +0900

    최댓값과 최솟값

--- programmers_level2/programmers_level2_1.java
+++ programmers_level2/programmers_level2_1.java
@@ -1,4 +1,33 @@
 package programmers_level2;
+//          최댓값과 최솟값
 
+
+//  문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다.
+//  str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+//  예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
 public class programmers_level2_1 {
+    public static String solution(String s) {
+        String[] a = s.split(" ");
+        int max,min;
+        max = min = Integer.parseInt(a[0]);
+        for(int i = 0; i < a.length; i++){
+            if(max < Integer.parseInt(a[i])){
+                max = Integer.parseInt(a[i]);
+            }
+            if(min > Integer.parseInt(a[i])){
+                min = Integer.parseInt(a[i]);
+            }
+        }
+
+        String answer = min + " " + max;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("1 2 3 4"));
+        System.out.println(solution("-1 -2 -3 -4"));
+        System.out.println(solution("-1 -1"));
+    }
+
 }
