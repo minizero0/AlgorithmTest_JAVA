c72313e85b7464ab51bf7c7a7d2352bf25f0f4f0
yeongmin <dudals1069@gmail.com>
2023-03-30 21:38:45 +0900

    숫자의 표현

---
+++ programmers_level2/programmers_level2_10.java
@@ -0,0 +1,27 @@
+package programmers_level2;
+//          숫자의 표현
+
+//  Finn은 요즘 수학공부에 빠져 있습니다.
+//  수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다.
+//  예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
+//  1 + 2 + 3 + 4 + 5 = 15
+//  4 + 5 + 6 = 15
+//  7 + 8 = 15
+//  15 = 15
+
+public class programmers_level2_10 {
+    public static int solution(int n) {
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j <= n; j++){
+                
+            }
+        }
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+}
