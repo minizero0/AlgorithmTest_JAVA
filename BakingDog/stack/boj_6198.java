70ec35883b7a058dc952072e0aed91e32a45349b
yeongmin <dudals1069@gmail.com>
2023-04-21 15:56:27 +0900

    stack

--- BakingDog/stack/boj_6198.java
+++ BakingDog/stack/boj_6198.java
@@ -10,18 +10,14 @@ public class boj_6198 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         Stack<Integer> stack = new Stack<>();
-        long count = 0;
-
+        int count = 0;
         for(int i = 0; i < n; i++){
             int num = Integer.parseInt(br.readLine());
-            while(!stack.isEmpty() && stack.peek() <= num){
-                stack.pop();
-            }
-            // 12 2
+
+            while(!stack.isEmpty() && stack.peek() <= num) stack.pop();
+            count += stack.size();
             stack.push(num);
-            count += stack.size()-1;
         }
         System.out.println(count);
     }
