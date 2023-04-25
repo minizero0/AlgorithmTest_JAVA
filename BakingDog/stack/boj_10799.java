69f273d352bd19f8a048997233a260ba61d0e021
yeongmin <dudals1069@gmail.com>
2023-04-25 15:40:03 +0900

    stack

--- BakingDog/stack/boj_10799.java
+++ BakingDog/stack/boj_10799.java
@@ -1,4 +1,29 @@
 package BakingDog.stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class boj_10799 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+
+        String s = br.readLine();
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') stack.push(s.charAt(i));
+            else{
+                char top = stack.pop();
+                if(s.charAt(i-1) == '('){
+                    ans += stack.size();
+                }else{
+                    ans += 1;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
 }
