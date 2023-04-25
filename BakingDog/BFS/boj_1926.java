4964813e077315b64335aa2d8e17aa2d0f5d592c
yeongmin <dudals1069@gmail.com>
2023-04-25 17:15:22 +0900

    BFS

---
+++ BakingDog/BFS/boj_1926.java
@@ -0,0 +1,64 @@
+package BakingDog.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_1926 {
+    static int n, m;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int[] dx;
+    static int[] dy;
+    static Queue<Pair> qu;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        visit = new boolean[n][m];
+        qu = new LinkedList<>();
+
+        dx = new int[] {1,0,-1,0};
+        dy = new int[] {0,1,0,-1};
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    public static class Pair{
+        int x, y;
+
+        public Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+}
