559c452dc4726344bf648b37951a7bfe08fb1311
yeongmin <dudals1069@gmail.com>
2023-04-26 10:08:48 +0900

    BFS

--- BakingDog/BFS/boj_1926Test.java
+++ BakingDog/BFS/boj_1926Test.java
@@ -1,16 +1,69 @@
 package BakingDog.BFS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class boj_1926Test {
+    static int n,m;
+    static int[][] arr;
+    static boolean[][] visit;
     static Queue<Pair> qu;
+    static int[] dx;
+    static int[] dy;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        visit = new boolean[n][m];
         qu = new LinkedList<>();
-        qu.offer(new Pair(1,2));
-        Pair p = qu.poll();
-        System.out.println(p.x + " " + p.y);
+        dx = new int[] {1,0,-1,0};
+        dy = new int[] {0,1,0,-1};
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int count = 0;
+        int area = 0;
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i][j] == 0 || visit[i][j]) continue;
+                count++;
+                qu.offer(new Pair(i,j));
+                visit[i][j] = true;
+                area = 0;
+
+                while(!qu.isEmpty()){
+                    Pair p = qu.poll();
+                    area++;
+                    for(int k = 0; k < 4; k++){
+                        int n_x = p.x + dx[k];
+                        int n_y = p.y + dy[k];
+                        if(n_x < 0 || n_x >= n || n_y < 0 || n_y >= m) continue;
+                        if(arr[n_x][n_y] == 1 && !visit[n_x][n_y]){
+                            qu.offer(new Pair(n_x,n_y));
+                            visit[n_x][n_y] = true;
+                        }
+                    }
+                }
+                if(area > max) max = area;
+            }
+        }
+        System.out.println(count);
+        System.out.println(max);
     }
 
 
