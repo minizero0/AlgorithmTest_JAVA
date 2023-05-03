2f0d2dad5212781b7134b528c6dbec643a18ee60
yeongmin <dudals1069@gmail.com>
2023-05-03 10:56:29 +0900

    boj_11729

--- BakingDog/recursion/boj_11729.java
+++ BakingDog/recursion/boj_11729.java
@@ -7,31 +7,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj_11729 {
-
     static StringBuilder sb = new StringBuilder();
     static int count = 0;
 
     public static void hanoi(int n, int from, int to, int other){
-        if (n == 0 ) return;
+        if(n == 0) return;
         else{
             count++;
-            hanoi(n-1, from, other, to);
+            hanoi(n-1,from,other,to);
 
-            sb.append(n + " " + from + " " + to + "\n");
+            sb.append(from + " " + to + "\n");
 
-            hanoi(n-1, other, to, from);
+            hanoi(n-1,other, to, from);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         hanoi(n,1,3,2);
         System.out.println(count);
         System.out.println(sb);
-
 
     }
 }
