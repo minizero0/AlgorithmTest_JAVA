b7f9288d69b0df3e7a189dae69d72dabde3a00c3
yeongmin <dudals1069@gmail.com>
2023-05-03 09:40:16 +0900

    boj_1629

--- BakingDog/recursion/boj_1629.java
+++ BakingDog/recursion/boj_1629.java
@@ -2,7 +2,30 @@ package BakingDog.recursion;
 
 // 자연수 A를 B번 곱한 수를 알고 싶다. 단 구하려는 수가 매우 커질 수 있으므로 이를 C로 나눈 나머지를 구하는 프로그램을 작성하시오.
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class boj_1629 {
 
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(pow(a,b,c));
+    }
+
+    public static long pow(long a, long b, long c){
+        if(b == 1) return a % c;
+
+        long value = pow(a, b/2, c);
+        value = value * value % c;
+        if(b%2 == 0) return value;
+        return value * a % c;
+    }
+
 }
