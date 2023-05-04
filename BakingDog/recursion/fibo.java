7d3b9e7842a6f0e9d425c66872e9a60602b005eb
yeongmin <dudals1069@gmail.com>
2023-05-04 16:53:52 +0900

    fb

--- BakingDog/recursion/fibo.java
+++ BakingDog/recursion/fibo.java
@@ -1,14 +1,14 @@
-package BakingDog.recursion;
+    package BakingDog.recursion;
 
-public class fibo {
+    public class fibo {
 
-    public static int fibo(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fibo(n-2) + fibo(n-1);
+        public static int fibo(int n){
+            if(n == 0) return 0;
+            if(n == 1) return 1;
+            return fibo(n-2) + fibo(n-1);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(fibo(10));
+        }
     }
-
-    public static void main(String[] args) {
-        System.out.println(fibo(10));
-    }
-}
