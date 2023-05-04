1bff42206d0c132973ef88358a7ea6f87e927a18
yeongmin <dudals1069@gmail.com>
2023-05-04 16:30:55 +0900

    boj_15649

---
+++ BakingDog/backtracking/boj_15649.java
@@ -0,0 +1,45 @@
+package BakingDog.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15649 {
+    static int N,M;
+    static boolean[] visit;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[N];
+        arr = new int[M];
+
+        dfs(N,M,0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int N, int M, int depth){
+        if(depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N,M,depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
