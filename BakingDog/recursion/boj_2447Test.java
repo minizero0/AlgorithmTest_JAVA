99de46fd92ce0e1ca981e3a00dca03402af912e4
yeongmin <dudals1069@gmail.com>
2023-05-04 15:04:00 +0900

    2447

--- BakingDog/recursion/boj_2447Test.java
+++ BakingDog/recursion/boj_2447Test.java
@@ -1,21 +1,21 @@
 package BakingDog.recursion;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj_2447Test {
-
     static char[][] arr;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         arr = new char[N][N];
 
-        star(0,0,N,false);
-        StringBuilder sb = new StringBuilder();
+        print(0,0,N,false);
+
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 sb.append(arr[i][j]);
@@ -23,35 +23,35 @@ public class boj_2447Test {
             sb.append("\n");
         }
         System.out.println(sb);
-
     }
 
-    static void star(int x, int y, int N, boolean blank){
+    static void print(int row, int col, int size, boolean blank){
         if(blank){
-            for(int i = x; i < x + N; i++){
-                for(int j = y; j < y + N; j++){
+            for(int i = row; i < row + size; i++){
+                for(int j = col; j < col + size; j++){
                     arr[i][j] = ' ';
                 }
             }
             return;
         }
 
-        if(N==1){
-            arr[x][y] = '*';
+        if(size == 1){
+            arr[row][col] = '*';
             return;
         }
 
-        int size = N / 3;
+        int newSize = size / 3;
         int count = 0;
-        for(int i = x; i < x + N; i += size){
-            for(int j = y; j < y + N; j += size){
+        for(int i = row; i < row + size; i += newSize){
+            for(int j = col; j < col + size; j += newSize) {
                 count++;
                 if(count == 5){
-                    star(i,j,size,true);
+                    print(i,j,newSize,true);
                 }else{
-                    star(i, j, size, false);
+                    print(i,j,newSize,false);
                 }
             }
         }
     }
+
 }
