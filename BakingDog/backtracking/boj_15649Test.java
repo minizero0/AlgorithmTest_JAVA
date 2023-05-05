4a18d85c80ed660a8170bd4182c03cb36f8070a8
yeongmin <dudals1069@gmail.com>
2023-05-05 11:04:28 +0900

    N and M

--- BakingDog/backtracking/boj_15649Test.java
+++ BakingDog/backtracking/boj_15649Test.java
@@ -6,15 +6,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class boj_15649Test {
-    static int N,M;
-    static boolean[] visit;
+    static int N, M;
     static int[] arr;
+    static boolean[] visit;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
@@ -26,7 +25,7 @@ public class boj_15649Test {
     }
 
     static void dfs(int N, int M, int depth){
-        if(depth == M){
+        if(M == depth){
             for(int val : arr){
                 sb.append(val).append(' ');
             }
@@ -37,12 +36,11 @@ public class boj_15649Test {
         for(int i = 0; i < N; i++){
             if(!visit[i]){
                 visit[i] = true;
-                arr[depth] = i + 1;
+                arr[depth] = i+1;
                 dfs(N,M,depth+1);
                 visit[i] = false;
             }
         }
+
     }
-
-
 }
