cd22ca9cabb3e7d22538ba02c4a53aeda62c9eb9
yeongmin <dudals1069@gmail.com>
2023-05-16 16:12:42 +0900

    swea

--- ssafy/s1954Test.java
+++ ssafy/s1954Test.java
@@ -8,7 +8,6 @@ public class s1954Test {
     static int T;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int dx[] = {0,1,0,-1};
         int dy[] = {1,0,-1,0};
 
@@ -20,20 +19,19 @@ public class s1954Test {
             int x = 0, y = 0;
             for(int i = 1; i <= n*n; i++){
                 arr[x][y] = i;
-                if(x+dx[loc] < 0 || x+dx[loc] >= n || y+dy[loc] < 0 || y+dy[loc] >= n || arr[x+dx[loc]][y+dy[loc]] != 0){
+                if(x + dx[loc] < 0 || x + dx[loc] >= n || y + dy[loc] < 0 || y+ dy[loc] >= n || arr[x+dx[loc]][y+dy[loc]] != 0){
                     loc = (loc + 1) % 4;
                 }
                 x += dx[loc];
                 y += dy[loc];
             }
             System.out.println("#"+t);
-            for(int i = 0; i < n; i++){
-                for(int j : arr[i]){
+            for(int i = 0; i < n; i++) {
+                for (int j : arr[i]){
                     System.out.print(j + " ");
                 }
                 System.out.println();
             }
         }
-
     }
 }
