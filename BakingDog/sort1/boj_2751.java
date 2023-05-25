d8911287acacfd4a4fb2ba83ebbe3e6984cb56bb
yeongmin <dudals1069@gmail.com>
2023-05-25 17:15:27 +0900

    수 정렬하기

---
+++ BakingDog/sort1/boj_2751.java
@@ -0,0 +1,23 @@
+package BakingDog.sort1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class boj_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arr);
+        for(int i = 0; i < arr.size(); i++){
+            bw.write(arr.get(i)+"\n");
+        }
+        bw.flush();
+    }
+}
