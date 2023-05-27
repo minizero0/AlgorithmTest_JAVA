5cfe7c147bec5f8e47eb809c0088bb2f83ed8bb1
yeongmin <dudals1069@gmail.com>
2023-05-27 18:53:32 +0900

    나이순 정렬하기

---
+++ BakingDog/sort1/boj_10814Test.java
@@ -0,0 +1,54 @@
+package BakingDog.sort1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class boj_10814Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Person[] p = new Person[N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            p[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
+        }
+
+        Arrays.sort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        for(int i = 0; i < N; i++){
+            sb.append(p[i]+"\n");
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+    public static class Person{
+        int age;
+        String name;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return age + " " + name;
+        }
+    }
+}
