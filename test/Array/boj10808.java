b7ed90ae961f254f6a51cd7f48b5e887be177964
yeongmin <dudals1069@gmail.com>
2023-06-15 16:22:22 +0900

    test

--- test/Array/boj10808.java
+++ test/Array/boj10808.java
@@ -1,10 +1,11 @@
 package test.Array;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj10808 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         int arr[] = new int[26];
