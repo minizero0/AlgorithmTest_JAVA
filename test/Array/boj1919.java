a5ac45615ee7150bd51611a0969e7d3b5c6a3a69
yeongmin <dudals1069@gmail.com>
2023-07-04 12:05:20 +0900

    test

--- test/Array/boj1919.java
+++ test/Array/boj1919.java
@@ -3,7 +3,6 @@ package test.Array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class boj1919 {
     public static void main(String[] args) throws IOException {
@@ -13,16 +12,12 @@ public class boj1919 {
 
         int arr[] = new int[26];
 
-
         for(int i = 0; i < a.length(); i++) arr[a.charAt(i)-'a']++;
         for(int i = 0; i < b.length(); i++) arr[b.charAt(i)-'a']--;
 
-
         int ans = 0;
 
-        for(int i = 0; i < arr.length; i++){
-            ans += Math.abs(arr[i]);
-        }
+        for(int i = 0; i < arr.length; i++) ans += Math.abs(arr[i]);
 
         System.out.println(ans);
     }
