04c5ad67168e0613465904f22d860dd5c7b1f91a
yeongmin <dudals1069@gmail.com>
2023-07-10 16:05:48 +0900

    stack

---
+++ test/Queue/boj2164.java
@@ -0,0 +1,25 @@
+package test.Queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class boj2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> queue = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+        while(queue.size() > 1){
+            queue.remove();
+            queue.add(queue.peek());
+            queue.remove();
+        }
+        System.out.println(queue.peek());
+    }
+}
