97e366d9645ce6d7f9e134da1d031f799d5ebe84
yeongmin <dudals1069@gmail.com>
2023-07-20 10:41:45 +0900

    stack

---
+++ test/Stack/boj1874Test.java
@@ -0,0 +1,36 @@
+package test.Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class boj1874Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 0;
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(start < num){
+                for(int j = start+1; j <= num; j++){
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                start = num;
+            }else if(stack.peek() != num){
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append("-\n");
+        }
+        System.out.println(sb);
+    }
+}
