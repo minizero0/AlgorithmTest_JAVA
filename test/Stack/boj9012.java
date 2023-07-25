f632c29e05f9e6682eb431645812c8fc95488a6c
yeongmin <dudals1069@gmail.com>
2023-07-25 17:44:23 +0900

    stack

---
+++ test/Stack/boj9012.java
@@ -0,0 +1,15 @@
+package test.Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+    }
+
+}
