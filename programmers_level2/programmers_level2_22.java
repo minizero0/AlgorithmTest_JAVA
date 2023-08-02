49318028055eb3f8480fe5442a94839ee2fe604f
yeongmin <dudals1069@gmail.com>
2023-08-02 16:57:54 +0900

    process

--- programmers_level2/programmers_level2_22.java
+++ programmers_level2/programmers_level2_22.java
@@ -2,45 +2,31 @@ package programmers_level2;
 
 //  프로세스
 
+import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class programmers_level2_22 {
-    int n1;
-    int n2;
-    public programmers_level2_22(int n1, int n2) {
-        n1 = this.n1;
-        n2 = this.n2;
-    }
-
-
     public static int solution(int[] priorities, int location) {
-        int answer = 0;
-        Deque<programmers_level2_22> queue = new LinkedList<>();
-
-        for(int i = 0; i < priorities.length; i++){
-            queue.offer(new programmers_level2_22(priorities[i], i));
-        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int n : priorities) queue.add(n);
 
         int count = 0;
-        int des = -1;
-        while(des != location){
-            int n = queue.peek().n1;
-            for(int i = 1; i < queue.size(); i++){
-            }
+        while(!queue.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(priorities[i] == queue.peek()){
+                    queue.poll();
+                    count++;
 
-            if(des != location){
-                queue.offer(queue.pop());
-                count++;
-            }else{
-                break;
+                    if(i == location) return count;
+                }
             }
         }
         return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[] {2,1,3,2},2));
+        System.out.println(solution(new int[] {2,1,3,2}, 2));
     }
-
 }
