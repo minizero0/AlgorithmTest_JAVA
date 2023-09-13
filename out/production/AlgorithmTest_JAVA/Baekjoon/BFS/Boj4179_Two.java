4070e9ae0ceda9b97621579a010f500492ff9c6b
yeongmin <dudals1069@gmail.com>
2023-09-13 17:05:34 +0900

    fire

--- out/production/AlgorithmTest_JAVA/Baekjoon/BFS/Boj4179_Two.java
+++ out/production/AlgorithmTest_JAVA/Baekjoon/BFS/Boj4179_Two.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Boj4179 {
+public class Boj4179_Two {
 
     static char[][] board;
     static int R, C, res;
@@ -16,18 +16,45 @@ public class Boj4179 {
     static Queue<State> fire;
     static Queue<State> jh;
 
-    private static boolean bfs() {
-        while (!jh.isEmpty()) {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        board = new char[R][C];
+        dx = new int[]{0, 1, 0, -1};
+        dy = new int[]{1, 0, -1, 0};
+        fire = new LinkedList<>();
+        jh = new LinkedList<>();
+
+        for (int i = 0; i < R; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = s.charAt(j);
+
+                if(board[i][j] == 'J') jh.add(new State(i, j, 0));
+                if(board[i][j] == 'F') fire.add(new State(i, j, 0));
+            }
+        }
+
+        if(bfs()) System.out.println(res);
+        else System.out.println("IMPOSSIBLE");
+    }
+
+    static boolean bfs() {
+        while (!jh.isEmpty()) {
             int size = fire.size();
+
             for (int i = 0; i < size; i++) {
                 State now = fire.poll();
 
                 for (int k = 0; k < 4; k++) {
                     int n_x = dx[k] + now.x;
                     int n_y = dy[k] + now.y;
-
-                    if(n_x < 0 || n_x >= R || n_y < 0 || n_y >= C) continue;
+                    if(n_x < 0 || n_y < 0 || n_x >= R || n_y >= C) continue;
                     if(board[n_x][n_y] == '#' || board[n_x][n_y] == 'F') continue;
 
                     board[n_x][n_y] = 'F';
@@ -35,7 +62,6 @@ public class Boj4179 {
                 }
             }
 
-            //지훈 불 피해 이동
             size = jh.size();
             for (int i = 0; i < size; i++) {
                 State now = jh.poll();
@@ -43,8 +69,7 @@ public class Boj4179 {
                 for (int k = 0; k < 4; k++) {
                     int n_x = dx[k] + now.x;
                     int n_y = dy[k] + now.y;
-
-                    if(n_x < 0 || n_x >= R || n_y < 0 || n_y >= C) {
+                    if(n_x < 0 || n_y < 0 || n_x >= R || n_y >= C) {
                         res = now.d + 1;
                         return true;
                     }
@@ -58,36 +83,7 @@ public class Boj4179 {
         return false;
     }
 
-
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        board = new char[R][C];
-        fire = new LinkedList<>();
-        jh = new LinkedList<>();
-        dx = new int[]{0, 1, 0, -1};
-        dy = new int[]{1, 0, -1, 0};
-
-        for (int i = 0; i < R; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = s.charAt(j);
-
-                if(board[i][j] =='J') jh.add(new State(i, j, 0));
-                if(board[i][j] == 'F') fire.add(new State(i, j, 0));
-            }
-        }
-        if(bfs()) System.out.println(res);
-        else System.out.println("IMPOSSIBLE");
-    }
-
-    public static class State {
+    static class State {
         int x, y, d;
 
         public State(int x, int y, int d) {
