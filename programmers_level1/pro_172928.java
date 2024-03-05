e503757ae88a6d6e18415c3d7be9b66929e37d2e
minizero0 <dudals1069@gmail.com>
2024-03-05 23:34:01 +0900

    2

--- programmers_level1/pro_172928.java
+++ programmers_level1/pro_172928.java
@@ -9,28 +9,95 @@ import java.util.Arrays;
         E : 동쪽으로 주어진 칸만큼 이동합니다.*/
 public class pro_172928 {
     public static int[] solution(String[] park, String[] routes) {
-        int[] answer = {};
         int H = 0;
         int W = 0;
 
-        int H_max = park.length;            //최대 세로 길이
-        int W_max = park[0].length();       //최대 가로 길이
-
-        for(int i = 0; i < routes.length; i++){
-            char c = routes[i].charAt(0);
-            H += routes[i].charAt(2) - 30;
-            if(c == 'N'){
-
+        for(int i = 0; i < park.length; i++){
+            for(int j = 0; j < park[i].length(); j++){
+                if(park[i].charAt(j) == 'S'){
+                    H = i;
+                    W = j;
+                }
             }
         }
 
 
 
+        int H_max = park.length;            //최대 세로 길이
+        int W_max = park[0].length();       //최대 가로 길이
 
-        return answer;
+        for (String route : routes) {
+            boolean flag = true;
+            char c = route.charAt(0);           //동서남북
+            int r = route.charAt(2) - 48;       //이동할 거리
+
+            System.out.println(c + "," + r );
+
+
+            if(c == 'N'){
+                if(H - r < 0) continue;
+                for(int i = 0; i < r; i++){
+                    if(park[H-i].charAt(W) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(!flag) continue;
+
+                H -= r;
+            }
+            if(c == 'S') {
+                if (H + r > H_max) continue;
+                for(int i = 0; i < r; i++){
+                    if(park.length == H + i){
+                        flag = false;
+                        break;
+                    }
+                    if(park[H+i].charAt(W) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(!flag) continue;
+                H += r;
+            }
+            if(c == 'W'){
+                if(W - r < 0) continue;
+                for(int i = 0; i < r; i++){
+                    if(park[H].charAt(W-i) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(!flag) continue;
+                W -= r;
+            }
+            if(c == 'E') {
+                if (W + r > W_max) continue;
+                for(int i = 0; i < r; i++){
+                    if(park[H].charAt(W+i) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(!flag) continue;
+                W += r;
+            }
+
+            System.out.println(H + "," + W );
+
+        }
+
+
+        return new int[]{H,W};
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new String[]{"SOO", "OOO", "OOO"}, new String[]{"E 2", "S 2", "W 1"})));
+        //System.out.println(Arrays.toString(solution(new String[]{"SOO", "OOO", "OOO"}, new String[]{"E 2", "S 2", "W 1"})));
+        //System.out.println(Arrays.toString(solution(new String[]{"SOO", "OXX", "OOO"}, new String[]{"E 2", "S 2", "W 1"})));
+        System.out.println(Arrays.toString(solution(new String[]{"OSO", "OOO", "OXO", "OOO"}, new String[]{"E 2", "S 3", "W 1"})));
     }
 }
