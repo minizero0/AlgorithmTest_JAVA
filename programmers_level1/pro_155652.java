1953ea39580417699b1acd40fd72efaadae920a3
minizero0 <dudals1069@gmail.com>
2024-03-07 23:23:14 +0900

    3 sol

--- programmers_level1/pro_155652.java
+++ programmers_level1/pro_155652.java
@@ -1,20 +1,24 @@
 package programmers_level1;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class pro_155652 {
     public static String solution(String s, String skip, int index) {
         String answer = "";
 
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            for(int j = 1; j <= index; j++){
+            for (int j = 0; j < index; j++) {
+                c += 1;
 
-                for(int k = 0; k < skip.length(); k++){
-                    if(skip.charAt(k) == c + j) c + 1;
-                }
+                if (c > 'z') c = 'a';                               //z가 넘어갈 경우 a로 초기화
+
+                if(skip.contains(String.valueOf(c))) j--;           //skip 단어가 포함되어 있는지 확인
             }
+            answer += c;
         }
-
 
         return answer;
     }
