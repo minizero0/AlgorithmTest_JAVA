dd5ca9174408f5ea31c280668806a38603620689
minizero0 <dudals1069@gmail.com>
2024-03-08 23:20:37 +0900

    4

---
+++ programmers_level1/pro_161989.java
@@ -0,0 +1,16 @@
+package programmers_level1;
+
+//덧칠하기
+public class pro_161989 {
+
+    public static int solution(int n, int m , int[] section){
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(8,4, new int[]{2,3,6}));
+    }
+}
