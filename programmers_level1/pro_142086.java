c9e1893b317749b2380c107de05c37d1686f6ca7
minizero0 <dudals1069@gmail.com>
2024-03-09 22:08:35 +0900

    6

---
+++ programmers_level1/pro_142086.java
@@ -0,0 +1,27 @@
+package programmers_level1;
+
+import java.util.Arrays;
+
+public class pro_142086 {
+    public static int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        answer[0] = -1;
+
+        for(int i = 1; i < s.length(); i++){
+            for(int j = i-1; j >= 0; j--){
+                if(s.charAt(i) == s.charAt(j)) {
+                    answer[i] = i - j;
+                    break;
+                }
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("banana")));
+        System.out.println(Arrays.toString(solution("foobar")));
+    }
+}
