52ea35e3e43396e780d17725e920d191999f5ca6
minizero0 <dudals1069@gmail.com>
2024-03-09 21:14:47 +0900

    4 sol

--- programmers_level1/pro_159994.java
+++ programmers_level1/pro_159994.java
@@ -4,30 +4,23 @@ package programmers_level1;
 public class pro_159994 {
     public static String solution(String[] cards1, String[] cards2, String[] goal) {
 
-        String answer = "";
-        for(String str : goal){
-            int a = 0;
-            int b = 0;
+        String answer = "Yes";
 
-            for (String string : cards1) {
-                if (string.equals(str)) {
-                    a = 1;
-                    break;
-                }
-            }
-            if(a == 1) continue;
+        int cards1_num = 0;
+        int cards2_num = 0;
 
-            for (String s : cards2) {
-                if (s.equals(str)) {
-                    b = 1;
-                    break;
-                }
+        for(String str : goal) {
+            if (str.equals(cards1[cards1_num])) {
+                if(cards1_num == cards1.length - 1) continue;
+                cards1_num++;
+                continue;
             }
-
-            if(a == 0 && b == 0){
-                answer = "NO";
-                break;
+            if (str.equals(cards2[cards2_num])) {
+                if(cards2_num == cards2.length - 1) continue;
+                cards2_num++;
+                continue;
             }
+            answer = "No";
         }
 
         return answer;
@@ -35,5 +28,6 @@ public class pro_159994 {
 
     public static void main(String[] args) {
         System.out.println(solution(new String[]{"i", "drink", "water"}, new String[]{"want","to"}, new String[]{"i", "want", "to", "drink", "water"}));
+        System.out.println(solution(new String[]{"i", "water", "drink"}, new String[]{"want","to"}, new String[]{"i", "want", "to", "drink", "water"}));
     }
 }
