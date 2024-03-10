366d9f9f5f9788d36613ad48bfe881a1eb2ea97f
minizero0 <dudals1069@gmail.com>
2024-03-10 21:52:30 +0900

    6 sol

--- programmers_level1/pro_134240.java
+++ programmers_level1/pro_134240.java
@@ -1,4 +1,21 @@
 package programmers_level1;
 
 public class pro_134240 {
+    public static String solution(int[] food) {
+        String answer = "0";
+
+        for(int i = food.length-1; i > 0; i--){
+            int n = food[i] / 2;
+
+            for(int j = 0; j < n; j++){
+                answer = i + answer + i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1, 3, 4, 6}));
+        System.out.println(solution(new int[] {1, 7, 1, 2}));
+    }
 }
