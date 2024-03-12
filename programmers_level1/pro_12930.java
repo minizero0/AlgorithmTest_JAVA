e3a892a38a040bc7c551716cf3cb17680c25544a
minizero0 <dudals1069@gmail.com>
2024-03-12 23:25:19 +0900

    8 so

--- programmers_level1/pro_12930.java
+++ programmers_level1/pro_12930.java
@@ -3,16 +3,20 @@ package programmers_level1;
 public class pro_12930 {
 
     public static String solution(String s) {
-        String answer = "";
+        StringBuffer answer = new StringBuffer();
 
-        String[] str = s.split(" ");
+        String[] str = s.split(" ", -1);
 
-        for(String a : str){
-            for(int i = 0; i < a.length(); i++){
-                
+        for(int i = 0; i < str.length; i++){
+            char[] charArr = str[i].toCharArray();
+            for(int j = 0; j < charArr.length; j++){
+                if(j % 2 == 0) charArr[j] = Character.toUpperCase(charArr[j]);
+                else charArr[j] = Character.toLowerCase(charArr[j]);
             }
+            if(i != str.length - 1) answer.append(charArr).append(" ");
+            else answer.append(charArr);
         }
-        return answer;
+        return String.valueOf(answer);
     }
 
     public static void main(String[] args) {
