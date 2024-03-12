79537d0ff79fc78c9743d9cf14b711847a8d44ce
minizero0 <dudals1069@gmail.com>
2024-03-12 22:55:39 +0900

    8

---
+++ programmers_level1/pro_12930.java
@@ -0,0 +1,21 @@
+package programmers_level1;
+
+public class pro_12930 {
+
+    public static String solution(String s) {
+        String answer = "";
+
+        String[] str = s.split(" ");
+
+        for(String a : str){
+            for(int i = 0; i < a.length(); i++){
+                
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("try hello world"));
+    }
+}
