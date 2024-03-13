d53df68d3eeff5c04f838882c483f9142e0423f5
minizero0 <dudals1069@gmail.com>
2024-03-13 23:11:03 +0900

    12915 sol

--- programmers_level1/pro_12915.java
+++ programmers_level1/pro_12915.java
@@ -1,8 +1,25 @@
 package programmers_level1;
 
+import java.util.*;
+
 public class pro_12915 {
-    public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        return answer;
+    public static String[] solution(String[] strings, int n) {
+        List<String> list = new ArrayList<>();
+
+        for (String string : strings) {
+            list.add(string.charAt(n) + string);
+        }
+
+        Collections.sort(list);         //list 정렬
+
+        for(int i = 0; i < list.size(); i++){
+            strings[i] = list.get(i).substring(1);
+        }
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[] {"sun", "bed", "car"}, 1)));
     }
 }
