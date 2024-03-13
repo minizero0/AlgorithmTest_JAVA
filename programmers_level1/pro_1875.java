6ad40772a03a8c7ab43dd53b00675d83d37e4610
minizero0 <dudals1069@gmail.com>
2024-03-13 22:43:24 +0900

    1875

---
+++ programmers_level1/pro_1875.java
@@ -0,0 +1,14 @@
+package programmers_level1;
+
+public class pro_1875 {
+    public static int solution(int[] nums){
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3,1,2,3}));
+        System.out.println(solution(new int[]{3,1,2,3}));
+        System.out.println(solution(new int[]{3,1,2,3}));
+    }
+}
