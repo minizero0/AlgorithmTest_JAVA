800b4b838ba306aff8efd1b1bc8ca5af6d26dd2c
minizero0 <dudals1069@gmail.com>
2024-03-13 22:47:20 +0900

    1875 sol

--- programmers_level1/pro_1875.java
+++ programmers_level1/pro_1875.java
@@ -1,14 +1,19 @@
 package programmers_level1;
 
+import java.util.HashSet;
+
 public class pro_1875 {
     public static int solution(int[] nums){
-        int answer = 0;
-        return answer;
+        HashSet<Integer> hash = new HashSet<>();
+
+        for(int n : nums) hash.add(n);
+
+        return Math.min(hash.size(), nums.length/2);
     }
 
     public static void main(String[] args) {
         System.out.println(solution(new int[]{3,1,2,3}));
-        System.out.println(solution(new int[]{3,1,2,3}));
-        System.out.println(solution(new int[]{3,1,2,3}));
+        System.out.println(solution(new int[]{3,3,3,2,2,4}));
+        System.out.println(solution(new int[]{3,3,3,2,2,2}));
     }
 }
