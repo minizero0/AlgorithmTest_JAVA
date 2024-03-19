78fdf5b33ecedbf1d107c6de1cbbc161f2b4bd3d
minizero0 <dudals1069@gmail.com>
2024-03-19 22:35:14 +0900

    136798

--- programmers_level1/pro_136798.java
+++ programmers_level1/pro_136798.java
@@ -1,12 +1,34 @@
 package programmers_level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class pro_136798 {
-    public int solution(int number, int limit, int power) {
+    public static int solution(int number, int limit, int power) {
         int answer = 0;
+
+        int[] arr = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            arr[i] = i;
+            int n = 0;
+
+            for (int j = 1; j * j <= arr[i]; j++) {
+                if(j*j == arr[i]) n++;
+                else if(arr[i] % j == 0) n += 2;
+                if (n > limit) {
+                    n = power;
+                    break;
+                }
+            }
+            answer += n;
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(solution(5, 3, 2));
+        System.out.println(solution(10, 3, 2));
     }
 }
