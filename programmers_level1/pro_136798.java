b71bbe0429f34e555d609c642531433aba78feae
minizero0 <dudals1069@gmail.com>
2024-03-19 22:03:28 +0900

    136798

---
+++ programmers_level1/pro_136798.java
@@ -0,0 +1,12 @@
+package programmers_level1;
+
+public class pro_136798 {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
