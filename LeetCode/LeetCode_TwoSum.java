117e35587c5134a4e26cc6f4c85f14273bdf1478
minizero0 <dudals1069@gmail.com>
2024-10-30 23:13:38 +0900

    hello

---
+++ LeetCode/LeetCode_TwoSum.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LeetCode_TwoSum {
+
+    public static int[] twoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
+    }
+}
