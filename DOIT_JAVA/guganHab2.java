cff380eaaea5aa01bdd2bab8245f30f50d27935b
minizero0 <dudals1069@gmail.com>
2025-09-09 20:19:49 +0900

    2차원 구간합

--- DOIT_JAVA/guganHab2.java
+++ DOIT_JAVA/guganHab2.java
@@ -1,4 +1,42 @@
 package DOIT_JAVA;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//합배열 만들기 
 public class guganHab2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String s = br.readLine();
+        st = new StringTokenizer(s);
+
+
+
+        int row = Integer.parseInt(st.nextToken());
+        int cnt = Integer.parseInt(st.nextToken());
+
+        int[][] a = new int[row+1][row+1];
+        int[][] d = new int[row + 1][row + 1]; //java는 배열 선언시 0으로 자동 초기화
+
+        for (int i = 1; i < row+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < row+1; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i < row+1; i++) {
+            for (int j = 1; j < row+1; j++) {
+                d[i][j] = d[i][j-1] + a[i][j] ;
+                System.out.print(d[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
