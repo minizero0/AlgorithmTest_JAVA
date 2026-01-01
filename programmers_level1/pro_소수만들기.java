245bfba3106c00aed21bb7fc788bbe100a715fe6
minizero0 <dudals1069@gmail.com>
2026-01-01 22:20:18 +0900

    HAPPY NEW YEAR

--- programmers_level1/pro_소수만들기.java
+++ programmers_level1/pro_소수만들기.java
@@ -1,2 +1,32 @@
-package programmers_level1;public class pro_소수만들기 {
+package programmers_level1;
+
+public class pro_소수만들기 {
+    private static boolean getPrime(int num) {
+        if(num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static int solution(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if(getPrime(nums[i] + nums[j] + nums[k])) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1,2,3,4}));
+    }
 }
--- programmers_level1/pro_콜라츠추측.java
+++ programmers_level1/pro_콜라츠추측.java
@@ -1,2 +1,22 @@
-package programmers_level1;public class pro_콜라츠추측 {
+package programmers_level1;
+
+public class pro_콜라츠추측 {
+    public static int solution(int n) {
+        int count = 0;
+        long num = n;
+        if(num == 1) return 0;
+
+        while (num != 1) {
+            count++;
+            if(count == 500) return -1;
+
+            if(num % 2 == 0) num /= 2;
+            else num = num * 3 + 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(626331));
+    }
 }
