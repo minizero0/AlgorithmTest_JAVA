14cbc197dfa656a479f47597189ad64bfb2cb69a
minizero0 <dudals1069@gmail.com>
2026-01-05 21:58:56 +0900

    택배상자

--- programmers_level1/pro_택배상자꺼내기.java
+++ programmers_level1/pro_택배상자꺼내기.java
@@ -5,13 +5,13 @@ public class pro_택배상자꺼내기 {
 
         public static int solution(int n, int w, int num) {
             // num의 층과 위치 인덱스
-            int row = (num - 1) / w;
-            int idx = (num - 1) % w;
+            int row = (num-1) / w;
+            int idx = (num-1) % w;
 
             int count = 1; // 자기 자신 포함
 
             // 위층 확인
-            for (int r = row + 1; ; r++) {
+            for (int r = row+1; ; r++) {
                 int start = r * w + 1;
                 if (start > n) break;
 
@@ -33,5 +33,6 @@ public class pro_택배상자꺼내기 {
     public static void main(String[] args) {
 //        22	6	8	3
         System.out.println(solution(22,6,8));
+        System.out.println(solution(3,6,4));
     }
 }
